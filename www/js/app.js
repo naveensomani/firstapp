6b170f1503034d74b2f0d978e8995614025f0b87
maisaengineering <contact@maisasolutions.com>
2014-07-07 11:56:19 +0530

    third commit

--- www/js/app.js
+++ www/js/app.js
@@ -2,12 +2,13 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
-   var homeTpl = Handlebars.compile($("#home-tpl").html());
-   var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+   HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+   EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+   
    var service = new EmployeeService();
-    service.initialize().done(function () {
-        renderHomeView();
-    });
+   service.initialize().done(function () {
+       $('body').html(new HomeView(service).render().$el);
+   });
 
     /* --------------------------------- Event Registration -------------------------------- */
      document.addEventListener('deviceready', function () {
@@ -27,14 +28,6 @@
         }
     }, false);
     /* ---------------------------------- Local Functions ---------------------------------- */
-   function findByName() {
-    service.findByName($('.search-key').val()).done(function (employees) {
-        $('.content').html(employeeListTpl(employees));
-    });
-   }
-   function renderHomeView() {
-    $('body').html(homeTpl());
-    $('.search-key').on('keyup', findByName);
-   }
+   
 
 }());
